ab6d165f31d71ce80a07c0652d53a134afa9140d
iTexZoz <lucenzo.76@gmail.com>
2021-07-16 21:58:10 +0200

    Implemented weapon infinite ammo.

--- src/menus/weapon/Weapon.lua
+++ src/menus/weapon/Weapon.lua
@@ -1,5 +1,7 @@
 local SELECTED_GROUP = nil;
 
+local INFINITE_AMMO_ENABLED = false;
+
 local MAIN_MENU = RageM.Get("RageM", "Main")
 RageM.New("Weapon", "Main", RageUI.CreateSubMenu(MAIN_MENU, "Weapon", "Weapon"))
 RageM.New("Weapon", "Selected", RageUI.CreateSubMenu(RageM.Get("Weapon", "Main"), "Weapon", "Weapon"))
@@ -11,6 +13,17 @@ end
 function RageUI.PoolMenus:Weapon()
 
     RageM.Get("Weapon", "Main"):IsVisible(function(Item)
+
+        Item:CheckBox("Infinite Ammo", nil, INFINITE_AMMO_ENABLED, {}, function(onSelected, onActive, IsChecked)
+            if (onSelected) then
+                INFINITE_AMMO_ENABLED = IsChecked
+                for i, v in pairs(WEAPONS) do
+                    SetPedInfiniteAmmo(Player.PedId, IsChecked, v.Hash)
+                end
+            end
+        end)
+
+        Item:AddSeparator("Weapon List")
         for i, v in pairs(WEAPONS_GROUP_AVAILABLE) do
             Item:AddButton(v, nil, {}, function(onSelected, onActive)
                 if (onSelected) then
@@ -30,7 +43,6 @@ function RageUI.PoolMenus:Weapon()
                 Item:AddButton(v.Weapon, nil, {}, function(onSelected, onActive)
                     if (onSelected) then
                         Native.GiveWeaponToPed(Player.PedId, v.Hash, 1, false, true, 1, false, 0.5, 1.0, "Weapon gived by RageM", 0)
-                        SetPedInfiniteAmmo(Player.PedId, true, v.Hash)
                     end
                 end)
             end
